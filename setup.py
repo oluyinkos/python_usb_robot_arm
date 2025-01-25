4243399ab70fdae26f00cd14de8bc56c49acd2bd
Olu Soboyede <dolapo@yinkos.com>
2025-01-25 19:57:05 +0000

    fix error package directory 'usb_robot_arm' does not exist

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='usb_robot_arm',
     version='0.2.0',
-    packages=['usb_robot_arm',],
+    packages=['usb_arm',],
     author="Danny Staple - Orionrobots",
     license='Creative Commons By Attribution Share-Alike v3.0',
     long_description=open('README.md').read(),
